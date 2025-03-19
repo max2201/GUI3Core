88852c9fcaec7d232020397ba3ed567996aaa0cf
max2201 <max22012201@gmail.com>
2025-03-19 17:30:39 +0700

    core init

--- src/core/api/configuration.api.ts
+++
@@ -1,9 +0,0 @@
-import type { IApiResponse } from '@/core/interface/Api'
-import type { IConfigurationDTO } from '@/core/interface/Configuration'
-
-// GetConfiguration
-export const GetConfiguration = async () => {
-  const { data, error }: IApiResponse<IConfigurationDTO> = await useApi('System.GetConfiguration')
-
-  return { data, error }
-}
